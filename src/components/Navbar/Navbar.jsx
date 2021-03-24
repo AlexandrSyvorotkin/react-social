b5dd9f942d81b3160ed87e1ab6b355dacbd97091
Alex <Aleks031994@mail.ru>
2021-03-24 23:19:56 +0300

    added Navlink functions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import s from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='/Profile'>Profile</a>
+                <NavLink to='/Profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
-            <div className={s.item}>
-                <a href='/Dialogs'>Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/Dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a href>News</a>
